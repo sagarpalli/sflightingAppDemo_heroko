49a6f3a30dcb4c226cded119600a041ecbb4d681
sagar <pallisagareee@gmail.com>
2019-03-24 22:20:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var nforce = require('nforce');
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var org = nforce.createConnection({
+  clientId: '3MVG9pe2TCoA1Pf5evZo8un4XI9BVKrsu0_Myqa7b6ciPeZi.f6WdYjjYv075UqfaEtu6ZnQAP0hTQAD0gmIi',
+  clientSecret: 'F5BDB3E4AFADF83331B4CE38BFEE0C13E19629916E5C784E0114B85F06E0F082',
+  redirectUri: 'https://protected-fortress-46904.herokuapp.com/oauth/_callback',
+  apiVersion: 'v34.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+
+var app = express();
+
+// Require Routes js
+var routesHome = require('./routes/home');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.use('/home', routesHome);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+  res.redirect(org.getAuthUri());
+});
+
+app.get('/oauth/_callback', function(req, res) {
+  org.authenticate({code: req.query.code}, function(err, resp){
+    if(!err) {
+      console.log('Access Token: ' + resp.access_token);
+      app.locals.oauthtoken = resp.access_token;
+      app.locals.lightningEndPointURI = "https://sedreambmo-dev-ed.lightning.force.com";
+      res.redirect('/home');
+    } else {
+      console.log('Error: ' + err.message);
+    }
+  });
+});
+
+// Served Localhost
+console.log('Served: http://localhost:' + port);
+app.listen(port);
